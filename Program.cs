6aedc5c25b6f8a64a195c0258db88848e2a01665
AlainProf <amartel@cstj.qc.ca>
2020-09-04 21:35:41 -0400

    Commit initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introAGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projet suivi avec Git");
+        }
+    }
+}
